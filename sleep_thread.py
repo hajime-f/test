712d71d95d258ffc5ba03ef7bd581b5345e06cc8
Hajime Fujita <hajime.fujita@gmail.com>
2021-07-15 16:18:28 +0900

    各スレッドがそれぞれ設定された秒数だけ待つようにした

--- sleep_thread.py
+++ sleep_thread.py
@@ -1,20 +1,33 @@
 import time
 import threading
 
-def worker(num):
-    print(time.time(), num)
+class Dog:
 
-def schedule(interval, f):
+    def __init__(self, n):
+        self.n = n
+
+    def bow(self):
+        print('bow-wow', self.n)
+
+        
+def dog_bow(num):
+
+    dog = Dog(num)
+    
     base_time = time.time()
     next_time = 0
+    n_itr = 0
+
     while True:
-        t = threading.Thread(target=f, args=(1,))
+        t = threading.Thread(target=dog.bow)
         t.start()
-        next_time = ((base_time - time.time()) % interval) or interval
+        next_time = ((base_time - time.time()) % num) or num
         time.sleep(next_time)
-
+        
+        
 if __name__ == '__main__':
-
-    schedule(1, worker)
-
-
+    
+    for i in range(1, 4):
+        t = threading.Thread(target=dog_bow, args=(i,))
+        t.start()
+        
