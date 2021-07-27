17eb3537ccba41efcba1c0706803c6bff81df7c1
Hajime Fujita <hajime.fujita@gmail.com>
2021-07-27 12:20:35 +0900

    sql-test

---
+++ read_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.finance as mpf
+
+import pdb
+
+if __name__ == '__main__':
+    
+    df = pd.read_csv("FRONTEO_20210720.csv")
+    
+    
+    
+    pdb.set_trace()
+    
