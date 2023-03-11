bc25fc1926a96eb1cdeba5e4c5751738ba4a61a8
hajime-f <hajime.fujita@gmail.com>
2023-03-11 22:14:07 +0900

    Livedoorニュースコーパスを追加

---
+++ analize_news.py
@@ -0,0 +1,44 @@
+"""Livedoorニュースを分類するプログラム"""
+
+import pandas as pd
+
+if __name__ == '__main__':
+
+    # データの読み込み
+    data = pd.read_csv('livedoornews.csv')
+
+    # 空のデータフレームを用意する
+    df = pd.DataFrame(columns=['words', 'category'])
+
+    for i in range(len(data)):
+
+        title = data.title[i].replace('\n', '')
+        body = data.body[i].replace('\n', '')
+
+        if data.media[i] == 'movie-enter':
+            category = 0
+        elif data.media[i] == 'it-life-hack':
+            category = 1
+        elif data.media[i] == 'kaden-channel':
+            category = 2
+        elif data.media[i] == 'topic-news':
+            category = 3
+        elif data.media[i] == 'livedoor-homme':
+            category = 4
+        elif data.media[i] == 'peachy':
+            category = 5
+        elif data.media[i] == 'sports-watch':
+            category = 6
+        elif data.media[i] == 'dokujo-tsushin':
+            category = 7
+        elif data.media[i] == 'smax':
+            category = 8
+        else:
+            category = -1
+
+        df_append = pd.DataFrame(data={'words': [title + body],
+                                       'category': [category]})
+        df = pd.concat([df, df_append], ignore_index=True)
+
+    # データの保存
+    df.to_csv('livedoor_news.csv', index=False)
